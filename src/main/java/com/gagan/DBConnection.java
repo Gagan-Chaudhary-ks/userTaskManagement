ba1b40ed33b759c3131b2ca602f11459ba486259
gagan <gaganjio14@gmail.com>
2026-02-17 15:18:00 +0530

    refactor: restructure main method with separate user and task menu

--- src/main/java/com/gagan/DBConnection.java
+++ src/main/java/com/gagan/DBConnection.java
@@ -19,65 +19,130 @@ public class DBConnection {
 
         Connection conn = DBConnection.getConnection();
         Scanner sc = new Scanner(System.in);
+
         UserService userService = new UserService(conn, sc);
+        TaskService taskService = new TaskService(conn, sc);
 
         while (true) {
 
-            System.out.println("\n1. Insert User");
-            System.out.println("2. View Users");
-            System.out.println("3. Delete User");
-            System.out.println("4. Exit");
+            System.out.println("\n===== MAIN MENU =====");
+            System.out.println("1. User Menu");
+            System.out.println("2. Task Menu");
+            System.out.println("3. Exit");
             System.out.print("Choose option: ");
 
-            int choice ;
-            if(sc.hasNextInt()){
+            int choice;
+
+            if (sc.hasNextInt()) {
                 choice = sc.nextInt();
                 sc.nextLine();
-            }else{
-                System.out.println("Invalid input. Please enter a number.");
-                sc.next();
+            } else {
+                System.out.println("Invalid input.");
+                sc.nextLine();
                 continue;
             }
 
             if (choice == 1) {
-                System.out.print("Enter name: ");
-                String name = sc.nextLine();
 
-                System.out.print("Enter email: ");
-                String email = sc.nextLine();
+                while (true) {
+                    System.out.println("\n--- USER MENU ---");
+                    System.out.println("1. Insert User");
+                    System.out.println("2. View Users");
+                    System.out.println("3. Delete User");
+                    System.out.println("4. Back");
+                    System.out.print("Choose option: ");
 
-                userService.insertUser(name, email);
+                    int userChoice;
+
+                    if (sc.hasNextInt()) {
+                        userChoice = sc.nextInt();
+                        sc.nextLine();
+                    } else {
+                        System.out.println("Invalid input.");
+                        sc.nextLine();
+                        continue;
+                    }
+
+                    if (userChoice == 1) {
+                        System.out.print("Enter name: ");
+                        String name = sc.nextLine();
+
+                        System.out.print("Enter email: ");
+                        String email = sc.nextLine();
+
+                        userService.insertUser(name, email);
+
+                    } else if (userChoice == 2) {
+                        userService.fetchUsers();
+
+                    } else if (userChoice == 3) {
+                        userService.deleteUser();
+
+                    } else if (userChoice == 4) {
+                        break;
+
+                    } else {
+                        System.out.println("Invalid option.");
+
+                    }
+                }
 
             } else if (choice == 2) {
-                System.out.println("\n");
-                userService.fetchUsers();
+
+                while (true) {
+                    System.out.println("\n--- TASK MENU ---");
+                    System.out.println("1. Add Task");
+                    System.out.println("2. View Tasks By User");
+                    System.out.println("3. Update Task Status");
+                    System.out.println("4. Delete Task");
+                    System.out.println("5. Back");
+                    System.out.print("Choose option: ");
+
+                    int taskChoice;
+
+                    if (sc.hasNextInt()) {
+                        taskChoice = sc.nextInt();
+                        sc.nextLine();
+                    } else {
+                        System.out.println("Invalid input.");
+                        sc.nextLine();
+                        continue;
+                    }
+
+                    if (taskChoice == 1) {
+                        taskService.addTask();
+
+                    } else if (taskChoice == 2) {
+                        taskService.viewTasksByUser();
+
+                    } else if (taskChoice == 3) {
+                        taskService.updateTaskStatus();
+
+                    } else if (taskChoice == 4) {
+                        taskService.deleteTask();
+
+                    } else if (taskChoice == 5) {
+                        break;
+
+                    } else {
+                        System.out.println("Invalid option.");
+
+                    }
+                }
 
             } else if (choice == 3) {
-                System.out.println("\n");
-                userService.fetchUsers();
-            System.out.print("Enter user ID to delete: ");
+                break;
 
-            if (sc.hasNextInt()) {
-                int id = sc.nextInt();
-                sc.nextLine();
-                userService.deleteUser(id);
             } else {
-                System.out.println("Invalid ID.");
-                sc.next();
+                System.out.println("Invalid option.");
+
             }
-
-        } else if (choice == 4) {
-
-            break;
         }
-          else{
-                System.out.println("Please choose a valid option !");
-                continue;
-            }
-    }
 
         sc.close();
+        conn.close();
     }
+
 
 
 }
